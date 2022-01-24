239c19514f1c7d0d45bfe8441ee56fe3ece7f0e9
yukiur <tida.cater.jack@gmail.com>
2022-01-24 18:12:40 +0900

    test

---
+++ src/servlet/SampleServlet.java
@@ -0,0 +1,42 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/SampleServlet")
+public class SampleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String[] luckArray = { "超スッキリ", "スッキリ", "最悪" };
+		int index = (int) (Math.random()*3);
+		String luck = luckArray[index];
+		
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM月dd日");
+		String today = sdf.format(date);
+		
+		//HTMLを出力
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>スッキリ占い</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<p>"+ today + "今日の運勢は「" + luck + "」です</p>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+}
